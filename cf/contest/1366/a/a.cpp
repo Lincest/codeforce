a02153247aaf3520e68f152499179f10fc1593b4
Lincest <474617032@qq.com>
2020-06-12 00:34:01 +0800

    1366 a

---
+++ cf/contest/1366/a/a.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author: RoccoShi
+ * Time: 2020-06-12 00:32:53
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--) {
+    	int a, b;
+    	cin >> a >> b;
+    	if(a >= 2*b || b >= 2*a)
+    		cout << min(a, b) << endl;
+       	else {
+       		if(a > b)
+       			swap(a,b);
+    		cout << (b-a) + (2*a-b)*2/3 <<endl;
+    	}
+    }
+	return 0;
+}
---
+++ cf/contest/1366/b/b.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: RoccoShi
+ * Time: 2020-06-12 00:32:53
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    return 0;
+}
